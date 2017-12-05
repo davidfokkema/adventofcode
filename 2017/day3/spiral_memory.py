31c69d866ce04599a580d874fa072984ad2be9c9
David Fokkema <davidfokkema@gmail.com>
2017-12-05 08:59:52 +0100

    Split the problem into pieces, and use generators

--- 2017/day3/spiral_memory.py
+++ 2017/day3/spiral_memory.py
@@ -1,13 +1,43 @@
+import itertools
+import time
+
+
 def number_of_steps(square_id):
-    if square_id == 1:
-        return 0
-    elif square_id == 12:
-        return 3
-    elif square_id == 23:
-        return 2
-    elif square_id == 1024:
-        return 31
+    x, y = get_coordinates_for_square(square_id)
+    return manhattan_distance(x, y)
+
+
+def get_coordinates_for_square(square_id):
+    x, y = 0, 0
+    for direction in itertools.islice(generate_spiral_steps(), 0,
+                                      square_id - 1):
+        if direction == 'RIGHT':
+            x += 1
+        elif direction == 'UP':
+            y += 1
+        elif direction == 'LEFT':
+            x -= 1
+        elif direction == 'DOWN':
+            y -= 1
+    return (x, y)
+
+
+def generate_spiral_steps():
+    directions = itertools.cycle(['RIGHT', 'UP', 'LEFT', 'DOWN'])
+    step_increase = itertools.cycle([0, 1])
+
+    number_of_steps = 1
+    for direction in directions:
+        for i in range(number_of_steps):
+            yield direction
+        number_of_steps += next(step_increase)
 
 
 def manhattan_distance(x, y):
     return abs(x) + abs(y)
+
+
+if __name__ == '__main__':
+    for step in generate_spiral_steps():
+        print(step)
+        time.sleep(.5)
--- 2017/day3/test_spiral_memory.py
+++ 2017/day3/test_spiral_memory.py
@@ -1,6 +1,8 @@
+import itertools
 import unittest
 
-from spiral_memory import number_of_steps, manhattan_distance
+from spiral_memory import (number_of_steps, manhattan_distance,
+                           get_coordinates_for_square, generate_spiral_steps)
 
 
 class SpiralMemoryTest(unittest.TestCase):
@@ -14,6 +16,16 @@ class SpiralMemoryTest(unittest.TestCase):
         self.assertEqual(manhattan_distance(0, 0), 0)
         self.assertEqual(manhattan_distance(2, 1), 3)
         self.assertEqual(manhattan_distance(0, -2), 2)
+
+    def test_get_coordinates_for_square(self):
+        self.assertEqual(get_coordinates_for_square(1), (0, 0))
+        self.assertEqual(get_coordinates_for_square(12), (2, 1))
+        self.assertEqual(get_coordinates_for_square(23), (0, -2))
+
+    def test_generate_spiral_steps(self):
+        steps = list(itertools.islice(generate_spiral_steps(), 0, 10))
+        self.assertEqual(steps, ['RIGHT'] + ['UP'] + 2 * ['LEFT'] +
+                         2 * ['DOWN'] + 3 * ['RIGHT'] + ['UP'])
 
 
 if __name__ == '__main__':
