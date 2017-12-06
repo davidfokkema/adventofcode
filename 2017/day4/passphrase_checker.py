d7e4496169dc2a1385b4ea77b08ccfebb33befb9
David Fokkema <davidfokkema@gmail.com>
2017-12-06 11:16:51 +0100

    Day 4: code golf
    Sigh. I should have thought of this. I didn’t. Thanks, @tomkooij !

--- 2017/day4/passphrase_checker.py
+++ 2017/day4/passphrase_checker.py
@@ -20,6 +20,25 @@ def is_secure(passphrase):
     return True
 
 
+# This is what I should have thought of. I was too busy writing tests and
+# almost literally implementing the logic of the puzzle's description. Of
+# course, Tom Kooij did think of this. While the following solution is less
+# verbose than the one above, the logic is not hard to follow. I like it.
+#
+# def is_valid(passphrase):
+#     words = passphrase.split(' ')
+#     # the set throws out duplicates
+#     return len(words) == len(set(words))
+#
+#
+# def is_secure(passphrase):
+#     words = passphrase.split(' ')
+#     # by sorting the letters in a word, anagrams become identical
+#     sorted_words = [''.join(sorted(u)) for u in words]
+#     # again, check for duplicates
+#     return len(sorted_words) == len(set(sorted_words))
+
+
 def count_valid_passphrases(filename):
     sum = 0
     with open(filename) as f:
