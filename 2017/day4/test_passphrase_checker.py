7b2cdeecccb525768dd06cb3827de992733a96ca
David Fokkema <davidfokkema@gmail.com>
2017-12-06 09:57:31 +0100

    OrderedDict is overkill

--- 2017/day4/test_passphrase_checker.py
+++ 2017/day4/test_passphrase_checker.py
@@ -1,23 +1,21 @@
-from collections import OrderedDict
 import unittest
 from unittest.mock import patch, mock_open, sentinel
 
 import passphrase_checker as pc
 
 
-# I like my tests ordered on difficulty level
-VALID_PASSPHRASES = OrderedDict([('aa bb cc dd ee', True),
-                                 ('aa bb cc dd aa', False),
-                                 ('aa bb cc dd aaa', True)])
+VALID_PASSPHRASES = [('aa bb cc dd ee', True),
+                     ('aa bb cc dd aa', False),
+                     ('aa bb cc dd aaa', True)]
 
 
 class PassphraseCheckerTest(unittest.TestCase):
     def test_passphrase_checker(self):
-        for phrase, is_valid in VALID_PASSPHRASES.items():
+        for phrase, is_valid in VALID_PASSPHRASES:
             self.assertEqual(pc.is_valid(phrase), is_valid)
 
     def test_count_valid_passphrases(self):
-        read_data = '\n'.join(VALID_PASSPHRASES.keys())
+        read_data = '\n'.join([u[0] for u in VALID_PASSPHRASES])
         mock = mock_open(read_data=read_data)
         with patch('passphrase_checker.open', mock):
             sum = pc.count_valid_passphrases(sentinel.filename)
