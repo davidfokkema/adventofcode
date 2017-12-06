44f0d786b5eac52d3626bd58fec1739053f9eaee
David Fokkema <davidfokkema@gmail.com>
2017-12-06 09:39:54 +0100

    Change test to use ordereddict of passphrases

--- 2017/day4/test_passphrase_checker.py
+++ 2017/day4/test_passphrase_checker.py
@@ -1,13 +1,19 @@
+from collections import OrderedDict
 import unittest
 
 import passphrase_checker as pc
 
 
+# I like my tests ordered on difficulty level
+VALID_PASSPHRASES = OrderedDict([('aa bb cc dd ee', True),
+                                 ('aa bb cc dd aa', False),
+                                 ('aa bb cc dd aaa', True)])
+
+
 class PassphraseCheckerTest(unittest.TestCase):
     def test_passphrase_checker(self):
-        self.assertTrue(pc.is_valid('aa bb cc dd ee'))
-        self.assertFalse(pc.is_valid('aa bb cc dd aa'))
-        self.assertTrue(pc.is_valid('aa bb cc dd aaa'))
+        for phrase, is_valid in VALID_PASSPHRASES.items():
+            self.assertEqual(pc.is_valid(phrase), is_valid)
 
 
 if __name__ == '__main__':
