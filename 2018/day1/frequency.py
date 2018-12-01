9a5ed7ebc892814591d9cf3c77d85d4fae88dafa
David Fokkema <davidfokkema@gmail.com>
2018-12-01 15:02:53 +0100

    Completed day 1, part 1

--- 2018/day1/frequency.py
+++ 2018/day1/frequency.py
@@ -3,3 +3,14 @@ def calculate_frequency(frequency_shifts):
     for shift in frequency_shifts:
         frequency += shift
     return frequency
+
+def read_shifts_from_file(filename):
+    with open(filename) as f:
+        shifts = [int(u) for u in f.readlines()]
+    return shifts
+
+
+if __name__ == '__main__':
+    shifts = read_shifts_from_file('./input.txt')
+    frequency = calculate_frequency(shifts)
+    print(f"Day 1: frequency is {frequency}")
--- 2018/day1/test_frequency.py
+++ 2018/day1/test_frequency.py
@@ -1,6 +1,7 @@
 import unittest
+from unittest.mock import patch, mock_open
 
-from frequency import calculate_frequency
+from frequency import calculate_frequency, read_shifts_from_file
 
 
 class FrequencyTest(unittest.TestCase):
@@ -9,6 +10,12 @@ class FrequencyTest(unittest.TestCase):
         self.assertEqual(calculate_frequency([+1, +1, +1]), 3)
         self.assertEqual(calculate_frequency([+1, +1, -2]), 0)
         self.assertEqual(calculate_frequency([-1, -2, -3]), -6)
+
+    def test_read_shifts_from_file(self):
+        mock = mock_open(read_data="-1\n-2\n+3\n")
+        with patch('frequency.open', mock):
+            shifts = read_shifts_from_file('foo.txt')
+        self.assertEqual(shifts, [-1, -2, 3])
 
 
 if __name__ == '__main__':
