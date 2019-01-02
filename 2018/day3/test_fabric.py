2880611bdcfa9e0386b920ea23db60e1d613ce85
David Fokkema <davidfokkema@gmail.com>
2019-01-02 21:48:53 +0100

    Converted functions into a class

--- 2018/day3/test_fabric.py
+++ 2018/day3/test_fabric.py
@@ -1,19 +1,22 @@
 import unittest
 
-from fabric import calculate_claim_size, calculate_fabric_size
+from fabric import Fabric
 
 
 claims = ['#1 @ 1,3: 4x4', '#2 @ 3,1: 4x4', '#3 @ 5,5: 2x2']
 
 
 class FabricTest(unittest.TestCase):
+    def setUp(self):
+        self.fabric = Fabric()
+
     def test_calculate_claim_size(self):
-        self.assertEqual(calculate_claim_size(claims[0]), (5, 7))
-        self.assertEqual(calculate_claim_size(claims[1]), (7, 5))
-        self.assertEqual(calculate_claim_size(claims[2]), (7, 7))
+        self.assertEqual(self.fabric.calculate_claim_size(claims[0]), (5, 7))
+        self.assertEqual(self.fabric.calculate_claim_size(claims[1]), (7, 5))
+        self.assertEqual(self.fabric.calculate_claim_size(claims[2]), (7, 7))
 
     def test_calculate_fabric_size(self):
-        self.assertEqual(calculate_fabric_size(claims), (7, 7))
+        self.assertEqual(self.fabric.calculate_fabric_size(claims), (7, 7))
 
 
 if __name__ == '__main__':
